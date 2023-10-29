55a62073b750c24661e20c446688f8aa841d79b7
fafiktheprogramist <glowaczra@gmail.com>
2023-10-29 22:47:23 +0100

    Created ProductController

---
+++ Controllers/ProductsController.cs
@@ -0,0 +1,40 @@
+﻿using AnkaraLab_BackEnd.WebAPI.DTOs;
+using AnkaraLab_BackEnd.WebAPI.Infrastructure;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AnkaraLab_BackEnd.WebAPI.Controllers
+{
+    [ApiController]
+    [Route("api/products")]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+    public class ProductsController : ControllerBase
+    {
+        private readonly IProductsRepository _productsRepository;
+        public ProductsController(IProductsRepository productsRepository)
+        {
+            _productsRepository = productsRepository ?? throw new ArgumentNullException(nameof(productsRepository));
+
+        }
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public ActionResult<IEnumerable<ProductDto>> GetProducts()
+        {
+            var products = _productsRepository.GetProducts();
+
+            return Ok(products);
+        }
+        [HttpGet("{id:int}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public ActionResult<ProductDto> GetProduct(int id) 
+        {
+            var product = _productsRepository.GetProduct(id);
+            if (product is null) 
+            { 
+                return NotFound(); 
+            }
+            return Ok(product);
+        }
+    }
+}
---
+++ DTOs/ProductDto.cs
@@ -0,0 +1,20 @@
+﻿using AnkaraLab_BackEnd.WebAPI.Domain;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace AnkaraLab_BackEnd.WebAPI.DTOs
+{
+    public class ProductDto
+    {
+        public int Id { get; }
+        public string Subcategory { get; set; } = string.Empty;
+        public string Size { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public double Price { get; set; }
+        public int Deadline { get; set; }
+        public bool IsAvaliable { get; set; }
+        public int PhotoHeight { get; set; }
+        public int PhotoWidth { get; set; }
+        public Category Category { get; set; } = default!;
+    }
+}
