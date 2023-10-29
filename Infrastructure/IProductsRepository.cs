a92cec058868c8e675104c96b9f8c2d36bc2ff85
glabojko <glabojko@gmail.com>
2023-10-29 21:35:08 +0100

    IproductRepository interface added

---
+++ Infrastructure/IProductsRepository.cs
@@ -0,0 +1,17 @@
+﻿using AnkaraLab_BackEnd.WebAPI.Domain;
+
+namespace AnkaraLab_BackEnd.WebAPI.Infrastructure
+{
+    public interface IProductsRepository
+    {
+        IEnumerable<Product> GetProducts();
+
+        Product GetProduct(int id);
+
+        void CreateProduct(Product product);
+
+        bool UpdateProduct(Product product);
+
+        bool DeleteProduct(int id);
+    }
+}
