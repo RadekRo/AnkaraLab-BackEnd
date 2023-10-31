a86cd6247c9514d675e01284beb27c9d46c8ab34
RadekRo <r.roclawski@ankara.pl>
2023-10-31 20:57:26 +0100

    Corrected Program.cs for db test connection

--- Program.cs
+++ Program.cs
@@ -1,4 +1,8 @@
 
+using AnkaraLab_BackEnd.WebAPI.Infrastructure;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+
 namespace AnkaraLab_BackEnd.WebAPI;
 
 public class Program
@@ -8,8 +12,20 @@ public class Program
         var builder = WebApplication.CreateBuilder(args);
 
         // Add services to the container.
+        builder.Services.AddDbContext<AnkaraLabDbContext>(options =>
+        {
+            options.UseSqlServer(builder.Configuration.GetConnectionString("AnkaraLabDb"));
+            options.EnableSensitiveDataLogging(builder.Environment.IsDevelopment());
+        });
 
-        builder.Services.AddControllers();
+        builder.Services.AddScoped<IProductsRepository, ProductsRepository>();
+        builder.Services.AddAutoMapper(Assembly.GetExecutingAssembly());
+
+        builder.Services.AddControllers().AddNewtonsoftJson(options =>
+        {
+            // required to prevent "Self referencing loop detected" error
+            options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
+        });
         // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
         builder.Services.AddEndpointsApiExplorer();
         builder.Services.AddSwaggerGen();
