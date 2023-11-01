47fc556dc435a13f4fd4d20bf3271638332a97fb
RadekRo <r.roclawski@ankara.pl>
2023-11-01 16:24:53 +0100

    Deleted WebSite from Product

--- Domain/Product.cs
+++ Domain/Product.cs
@@ -9,8 +9,6 @@ namespace AnkaraLab_BackEnd.WebAPI.Domain
         [Key]
         public int Id { get; }
         [Required]
-        public string WebPage { get; } = "ALab";
-        [Required]
         public string Size { get; set; } = string.Empty;
         [Required]
         [StringLength(250)]
