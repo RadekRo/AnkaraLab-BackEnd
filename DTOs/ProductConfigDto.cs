93f742b406141eb9cca7545c18d47b6b2fb23b3d
RadekRo <r.roclawski@ankara.pl>
2023-11-11 21:27:32 +0100

    Created Product Configuration DTO

--- DTOs/ProductConfigDto.cs
+++ DTOs/ProductConfigDto.cs
@@ -4,6 +4,8 @@
     {
         int Id { get; set; }
         string Name {  get; set; } = string.Empty;
+        int SQMPrice { get; set; }
+        int OptionLevel { get; set; }
         int CategoryId { get; set; }
     }
 }
