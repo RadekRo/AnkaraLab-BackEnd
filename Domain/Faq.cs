5e7fe942c84bf898ea304bd374e587fbe1f40191
glabojko <glabojko@gmail.com>
2023-11-12 19:56:19 +0100

    class Faq added in domain

---
+++ Domain/Faq.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AnkaraLab_BackEnd.WebAPI.Domain
+{
+    public class Faq
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required]
+        public string Question { get; set; }
+        [Required]
+        public string Answer { get; set; }
+    }
+}
