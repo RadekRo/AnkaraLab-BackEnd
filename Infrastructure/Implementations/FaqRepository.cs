ad003c926f9f84a9c233efa20d782ca11b99a81a
glabojko <glabojko@gmail.com>
2023-11-12 21:08:55 +0100

    funtions create and delete added in FaqRepository

--- Infrastructure/Implementations/FaqRepository.cs
+++ Infrastructure/Implementations/FaqRepository.cs
@@ -12,22 +12,42 @@ namespace AnkaraLab_BackEnd.WebAPI.Infrastructure.Implementations
         }
         public void CreateAnswer(Faq answer)
         {
-            throw new NotImplementedException();
+            _dbContext.Faqs.Add(answer);
+            _dbContext.SaveChanges();
         }
 
         public void CreateQuestion(Faq question)
         {
-            throw new NotImplementedException();
+            _dbContext.Faqs.Add(question);
+            _dbContext.SaveChanges();
         }
 
         public bool DeleteAnswer(int id)
         {
-            throw new NotImplementedException();
+            var answer = _dbContext.Faqs.SingleOrDefault(a => a.Id == id);
+
+            if (answer is null)
+            {
+                return false;
+            }
+            _dbContext.Faqs.Remove(answer);
+            _dbContext.SaveChanges();
+
+            return true;
         }
 
         public bool DeleteQuestion(int id)
         {
-            throw new NotImplementedException();
+            var question = _dbContext.Faqs.SingleOrDefault(q => q.Id == id);
+
+            if (question is null)
+            {
+                return false;
+            }
+            _dbContext.Faqs.Remove(question);
+            _dbContext.SaveChanges();
+
+            return true;
         }
 
         public bool UpdateAnswer(int id, Faq answer)
