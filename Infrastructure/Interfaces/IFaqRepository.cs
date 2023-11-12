11be1166df5f01a01870de1c7ffd941b0574fe66
glabojko <glabojko@gmail.com>
2023-11-12 20:22:47 +0100

    interface IfaqRepository added in Infrastructure

---
+++ Infrastructure/Interfaces/IFaqRepository.cs
@@ -0,0 +1,14 @@
+﻿using AnkaraLab_BackEnd.WebAPI.Domain;
+
+namespace AnkaraLab_BackEnd.WebAPI.Infrastructure.Interfaces
+{
+    public interface IFaqRepository
+    {
+        void CreateQuestion(Faq question);
+        void CreateAnswer(Faq answer);
+        bool UpdateQuestion(int id, Faq question);
+        bool UpdateAnswer(int id, Faq answer);
+        bool DeleteQuestion(int id);
+        bool DeleteAnswer(int id);
+    }
+}
