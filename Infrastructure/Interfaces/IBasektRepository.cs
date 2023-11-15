bc8e07c1bafbd3ae97d5e7453abdc12da9a07541
glabojko <glabojko@gmail.com>
2023-11-15 23:44:01 +0100

    IBasketRepository interface added

---
+++ Infrastructure/Interfaces/IBasektRepository.cs
@@ -0,0 +1,12 @@
+﻿using AnkaraLab_BackEnd.WebAPI.Domain;
+
+namespace AnkaraLab_BackEnd.WebAPI.Infrastructure.Interfaces
+{
+    public interface IBasektRepository
+    {
+        Basket? GetBasket(int id);
+        void CreateBaskeq(Basket basket);
+        bool UpdateBasket(Basket basket);
+        bool DeleteBasket(int id);
+    }
+}
