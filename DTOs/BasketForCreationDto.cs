7a35985f13bb70a3e155d784dee5739e904e9f85
glabojko <glabojko@gmail.com>
2023-11-15 23:59:32 +0100

    BasketForCreationDto class added

---
+++ DTOs/BasketForCreationDto.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AnkaraLab_BackEnd.WebAPI.DTOs
+{
+    public class BasketForCreationDto
+    {
+        [Required]
+        public int ClientId { get; set; }
+        [Required]
+        public int ProductId { get; set; }
+        [Required]
+        public int Quantity { get; set; }
+        [Required]
+        public int OrderId { get; set; }
+    }
+}
