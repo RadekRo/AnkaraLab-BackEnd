d4af2a98ab2bd59abec63856deac968c268d33ea
glabojko <glabojko@gmail.com>
2023-11-15 23:38:57 +0100

    BasketProfile class added

---
+++ Configurations/Mapper/BasektProfile.cs
@@ -0,0 +1,14 @@
+﻿using AnkaraLab_BackEnd.WebAPI.Domain;
+using AnkaraLab_BackEnd.WebAPI.DTOs;
+using AutoMapper;
+
+namespace AnkaraLab_BackEnd.WebAPI.Configurations.Mapper
+{
+    public class BasektProfile : Profile
+    {
+        public BasektProfile()
+        {
+            CreateMap<Basket, BasketDto>();
+        }
+    }
+}
