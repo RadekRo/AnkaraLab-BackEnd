3e8ab032b3c526c90938616d89ea2aa59e810b54
glabojko <glabojko@gmail.com>
2023-11-16 00:10:05 +0100

    BasketController class added

---
+++ Controllers/BasketsController.cs
@@ -0,0 +1,69 @@
+﻿using AnkaraLab_BackEnd.WebAPI.Domain;
+using AnkaraLab_BackEnd.WebAPI.DTOs;
+using AnkaraLab_BackEnd.WebAPI.Infrastructure.Implementations;
+using AnkaraLab_BackEnd.WebAPI.Infrastructure.Interfaces;
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AnkaraLab_BackEnd.WebAPI.Controllers
+{
+    [ApiController]
+    [Route("api/baket")]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+    public class BasketsController : ControllerBase
+    {
+        private readonly IBasketRepository _basketRepository;
+        private readonly IMapper _mapper;
+        public BasketsController(IBasketRepository basketRepository, IMapper mapper)
+        {
+            _basketRepository = basketRepository ?? throw new ArgumentNullException(nameof(basketRepository));
+            _mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
+        }
+
+        [HttpGet("{id:int}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public ActionResult<BasketDto> GetBasket(int id)
+        {
+            var basket= _basketRepository.GetBasket(id);
+            if (basket is null)
+            {
+                return NotFound();
+            }
+            return Ok(basket);
+        }
+
+        [HttpDelete("api/basket/delete/{id:int}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public ActionResult DeleteBasket(int id)
+        {
+            var basketToDelete = _basketRepository.DeleteBasket(id);
+            if (basketToDelete == false)
+            {
+                return NotFound("There is no such item");
+            }
+            return Ok(basketToDelete);
+        }
+
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public IActionResult CreateBasket([FromBody] BasketForCreationDto basketForCreationDto)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var basket = _mapper.Map<Basket>(basketForCreationDto);
+
+            _basketRepository.CreateBasket(basket);
+
+            var basketDto = _mapper.Map<FaqDto>(basket);
+
+            return CreatedAtAction(nameof(GetBasket), new { id = basket.Id }, basketDto);
+        }
+    }
+}
