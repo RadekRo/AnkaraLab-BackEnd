257bd3ba607866f66135b4e9bafc6f63da2c5971
glabojko <glabojko@gmail.com>
2023-11-16 01:14:18 +0100

    database migratoin refresh2 and update to recover missing faqs

--- Domain/Basket.cs
+++ Domain/Basket.cs
@@ -5,7 +5,10 @@ namespace AnkaraLab_BackEnd.WebAPI.Domain
 {
     public class Basket
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
+        [Required]
         public int ClientId { get; set; }
         [Required]
         [ForeignKey(nameof(Product))]
