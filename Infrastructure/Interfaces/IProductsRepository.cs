b47a8d744165ce8892b26b06c7e2dfc7c672b5e5
glabojko <glabojko@gmail.com>
2023-11-21 22:33:18 +0100

    fix: put endpoint add product fixed

--- Infrastructure/Interfaces/IProductsRepository.cs
+++ Infrastructure/Interfaces/IProductsRepository.cs
@@ -6,7 +6,7 @@ namespace AnkaraLab_BackEnd.WebAPI.Infrastructure.Interfaces
     public interface IProductsRepository
     {
         Task<Product?> GetProductAsync(int id);
-        Task CreateProduct(Product product);
+        Task CreateProductAsync(Product product);
         Task<bool> UpdateProductAsync(Product product);
         Task<bool> DeleteProductAsync(int id);
         Task<IEnumerable<Product>> GetProductsByCategoryAsync(int categoryId);
