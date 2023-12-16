7e676a339928c453503fdd049e1f243fe2a097ff
glabojko <glabojko@gmail.com>
2023-12-17 00:01:55 +0100

    ClientsController class added

---
+++ Controllers/ClientsController.cs
@@ -0,0 +1,82 @@
+﻿using AnkaraLab_BackEnd.WebAPI.Domain;
+using AnkaraLab_BackEnd.WebAPI.DTOs;
+using AnkaraLab_BackEnd.WebAPI.Infrastructure.Implementations;
+using AnkaraLab_BackEnd.WebAPI.Infrastructure.Interfaces;
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Net;
+
+namespace AnkaraLab_BackEnd.WebAPI.Controllers
+{
+    [ApiController]
+    [Route("api/client")]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+    public class ClientsController : ControllerBase
+    {
+        private readonly IClientRepository _clientRepository;
+        private readonly IMapper _mapper;
+        private readonly ILogger<ClientsController> _logger;
+        public ClientsController(IClientRepository clientRepository, IMapper mapper, ILogger<ClientsController> logger)
+        {
+            _clientRepository = clientRepository ?? throw new ArgumentNullException(nameof(clientRepository));
+            _mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+        }
+
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public async Task<ActionResult<IEnumerable<ClientDto>>> GetClients()
+        {
+            var clients = await _clientRepository.GetClientsAsync();
+            _logger.LogInformation("Estabilished connection with database. Retrieved all clients:");
+            return Ok(clients);
+        }
+
+        [HttpGet("{id:int}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<ActionResult<ClientDto>> GetClient(int id)
+        {
+            var client = await _clientRepository.GetClientAsync(id);
+            if (client is null)
+            {
+                return NotFound();
+            }
+            return Ok(client);
+        }
+
+        [HttpDelete("delete/{id:int}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<ActionResult> DeleteClient(int id)
+        {
+            var clientToDelete = await _clientRepository.DeleteClientAsync(id);
+            if (clientToDelete == false)
+            {
+                return NotFound("There is no such item");
+            }
+            return Ok(clientToDelete);
+        }
+
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<IActionResult> CreateClient([FromBody] ClientForCreationDto clientForCreationDto)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var client = _mapper.Map<Client>(clientForCreationDto);
+
+            await _clientRepository.CreateClientAsync(client);
+
+            var clientDto = _mapper.Map<ClientDto>(client);
+
+            return CreatedAtAction(nameof(GetClient), new { id = client.Id }, clientDto);
+        }
+    }
+}
