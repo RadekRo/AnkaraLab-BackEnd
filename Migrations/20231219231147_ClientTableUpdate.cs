eae37e1b2ca7ca195109b9d0763fd81a05326298
glabojko <glabojko@gmail.com>
2023-12-20 00:16:48 +0100

    FIX: column in Clients table changed database updated

---
+++ Migrations/20231219231147_ClientTableUpdate.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AnkaraLab_BackEnd.WebAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class ClientTableUpdate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Login",
+                table: "Clients",
+                newName: "Email");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Email",
+                table: "Clients",
+                newName: "Login");
+        }
+    }
+}
