d97082670b6a10162602d76cc9fbae36f4571105
glabojko <glabojko@gmail.com>
2024-02-10 17:45:28 +0100

    FEAT: class NewsletterForCreationDto added

--- Configurations/Mapper/NewsletterProfile.cs
+++ Configurations/Mapper/NewsletterProfile.cs
@@ -1,6 +1,15 @@
-﻿namespace AnkaraLab_BackEnd.WebAPI.Configurations.Mapper
+﻿using AnkaraLab_BackEnd.WebAPI.Domain;
+using AnkaraLab_BackEnd.WebAPI.DTOs;
+using AutoMapper;
+
+namespace AnkaraLab_BackEnd.WebAPI.Configurations.Mapper
 {
-    public class NewsletterProfile
+    public class NewsletterProfile : Profile
     {
+        public NewsletterProfile()
+        {
+            CreateMap<Newsletter, NewsletterDto>();
+            CreateMap<NewsletterForCreationDto, Newsletter>();
+        }
     }
 }
--- Controllers/NewslettersController.cs
+++ Controllers/NewslettersController.cs
@@ -1,6 +1,25 @@
-﻿namespace AnkaraLab_BackEnd.WebAPI.Controllers
+﻿using AnkaraLab_BackEnd.WebAPI.Domain;
+using AnkaraLab_BackEnd.WebAPI.DTOs;
+using AnkaraLab_BackEnd.WebAPI.Infrastructure.Interfaces;
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AnkaraLab_BackEnd.WebAPI.Controllers
 {
-    public class NewslettersController
+    [ApiController]
+    [Route("api/newsletter")]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+    public class NewslettersController : ControllerBase
     {
+        private readonly INewsletterRepository _newsletterRepository;
+        private readonly IMapper _mapper;
+        private readonly ILogger<NewslettersController> _logger;
+        public NewslettersController(INewsletterRepository newsletterRepository, IMapper mapper, ILogger<NewslettersController> logger)
+        {
+            _newsletterRepository = newsletterRepository ?? throw new ArgumentNullException(nameof(newsletterRepository));
+            _mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+        }
     }
 }
