fac325fa4e2eeb466beb6974615a0e10cc341980
glabojko <glabojko@gmail.com>
2024-02-10 18:18:31 +0100

    FEAT: CRUD functions in NewsletterController added

--- Controllers/NewslettersController.cs
+++ Controllers/NewslettersController.cs
@@ -1,5 +1,6 @@
 ﻿using AnkaraLab_BackEnd.WebAPI.Domain;
 using AnkaraLab_BackEnd.WebAPI.DTOs;
+using AnkaraLab_BackEnd.WebAPI.Infrastructure.Implementations;
 using AnkaraLab_BackEnd.WebAPI.Infrastructure.Interfaces;
 using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
@@ -20,6 +21,51 @@ namespace AnkaraLab_BackEnd.WebAPI.Controllers
             _newsletterRepository = newsletterRepository ?? throw new ArgumentNullException(nameof(newsletterRepository));
             _mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
             _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+        }
+
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<IActionResult> AddNewsletter([FromBody] NewsletterForCreationDto newsletterForCreationDto)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var newsletter = _mapper.Map<Newsletter>(newsletterForCreationDto);
+
+            await _newsletterRepository.AddNewsletterAsync(newsletter);
+
+            var newsletterDto = _mapper.Map<FaqDto>(newsletter);
+
+            return CreatedAtAction(nameof(GetNewsletter), new { id = newsletter.Id }, newsletterDto);
+        }
+
+        [HttpGet("{id:int}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<ActionResult<NewsletterDto>> GetNewsletter(int id)
+        {
+            var newsletter = await _newsletterRepository.GetNewsletterAsync(id);
+            if (newsletter is null)
+            {
+                return NotFound();
+            }
+            return Ok(newsletter);
+        }
+
+        [HttpDelete("delete/{id:int}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<ActionResult> DeleteNewsletter(int id)
+        {
+            var newsletterToDelete = await _newsletterRepository.DeleteNewsletterAsync(id);
+            if (newsletterToDelete == false)
+            {
+                return NotFound("There is no such item");
+            }
+            return Ok(newsletterToDelete);
         }
     }
 }
--- Infrastructure/Interfaces/INewsletterRepository.cs
+++ Infrastructure/Interfaces/INewsletterRepository.cs
@@ -4,6 +4,7 @@ namespace AnkaraLab_BackEnd.WebAPI.Infrastructure.Interfaces
 {
     public interface INewsletterRepository
     {
+        Task<Newsletter?> GetNewsletterAsync(int id);
         Task AddNewsletterAsync(Newsletter newsletter);
         Task<bool> DeleteNewsletterAsync(int id);
     }
