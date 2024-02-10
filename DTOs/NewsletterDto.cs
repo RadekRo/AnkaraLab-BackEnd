4c4264aadcb0d65eb7562b58f844ecd17ed6abbd
glabojko <glabojko@gmail.com>
2024-02-10 17:22:32 +0100

    FEAT: class NewsletterDto added

---
+++ DTOs/NewsletterDto.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AnkaraLab_BackEnd.WebAPI.DTOs
+{
+    public class NewsletterDto
+    {
+        public int Id { get; set; }
+        public string? Email { get; set; } = string.Empty;
+    }
+}
--- Domain/Newsletter.cs
+++ Domain/Newsletter.cs
@@ -1,6 +1,14 @@
-﻿namespace AnkaraLab_BackEnd.WebAPI.Domain
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AnkaraLab_BackEnd.WebAPI.Domain
 {
     public class Newsletter
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        [Required]
+        public string? Email { get; set; }
     }
 }
