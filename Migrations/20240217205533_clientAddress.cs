e69546f731b3d5d9f9516dae2cf51ae5d62fc6ed
glabojko <glabojko@gmail.com>
2024-02-17 22:29:42 +0100

    FEAT: properties with address added to client.cs

---
+++ Migrations/20240217205533_clientAddress.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AnkaraLab_BackEnd.WebAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class clientAddress : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ApartamentNumber",
+                table: "Clients",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "City",
+                table: "Clients",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Country",
+                table: "Clients",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "HouseNumber",
+                table: "Clients",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Street",
+                table: "Clients",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "ZipCode",
+                table: "Clients",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ApartamentNumber",
+                table: "Clients");
+
+            migrationBuilder.DropColumn(
+                name: "City",
+                table: "Clients");
+
+            migrationBuilder.DropColumn(
+                name: "Country",
+                table: "Clients");
+
+            migrationBuilder.DropColumn(
+                name: "HouseNumber",
+                table: "Clients");
+
+            migrationBuilder.DropColumn(
+                name: "Street",
+                table: "Clients");
+
+            migrationBuilder.DropColumn(
+                name: "ZipCode",
+                table: "Clients");
+        }
+    }
+}
